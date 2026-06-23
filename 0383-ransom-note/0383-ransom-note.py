3e0266e71bc599052d663d01a504ddaa6dc58620
Raghav Kedia <raghavkedia2205@gmail.com>
2026-06-24 00:29:12 +0530

    Time: 24 ms (41.91%), Space: 19.5 MB (93.72%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count={}
+
+        for ch in magazine:
+            count[ch] = count.get(ch,0)+1
+
+        for ch in ransomNote:
+            if ch not in count:
+                return False
+            
+            count[ch]-=1
+        
+            if count[ch]<0:
+                return False
+
+        return True
+        
